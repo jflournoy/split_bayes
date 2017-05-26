aedfd344b513476e6e2b8a494178f34dbe4e6c33
John Flournoy <flournoy@uoregon.edu>
2017-05-26 11:16:54 -0700

    array job

--- run_stan_models.bash
+++ run_stan_models.bash
@@ -1,15 +1,15 @@
 #!/bin/bash
 #
 #SBATCH --job-name=run_stan_model
-#SBATCH --output=run_stan_model.log
+#SBATCH --output=run_stan_model_%A_%a.log
 #
 #SBATCH --cpus-per-task=28
-#SBATCH --ntasks=3
-#SBATCH --mem-per-cpu=1000
+#SBATCH --ntasks=1
+#SBATCH --mem-per-cpu=2000
 #SBATCH --partition=defq,fat,long,longfat
+
+models=(run_stan_ht_model.r run_stan_dl_model.r run_stan_pu_model.r)
 
 module load R gcc
 
-srun Rscript --verbose run_stan_ht_model.r
-srun Rscript --verbose run_stan_dl_model.r
-srun Rscript --verbose run_stan_pu_model.r
+srun Rscript --verbose ${models[$SLURM_ARRAY_TASK_ID]}
