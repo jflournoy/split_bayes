5397bee249df593f93f2dbc114bc84e503eb941c
John Flournoy <flournoy@uoregon.edu>
2017-05-26 11:16:44 -0700

    better faster stronger

--- run_stan_dl_model.r
+++ run_stan_dl_model.r
@@ -6,5 +6,5 @@ options(mc.cores = parallel::detectCores())
 
 dlRW_m1_fit <- stan(file = file.path(stan_model_dir, 'gng_m1_reg.stan'), 
                     data = dl_stan_data, 
-                    iter = 7500, warmup = 5000, chains = 28, control = list(adapt_delta = 0.90), init = genInitListFunc(dl_stan_data$N))
+                    iter = 7500, warmup = 5000, chains = 28, control = list(adapt_delta = 0.95), init = genInitListFunc(dl_stan_data$N))
 saveRDS(dlRW_m1_fit, dlRW_m1_fname)
