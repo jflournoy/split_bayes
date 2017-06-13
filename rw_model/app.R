2f4f53d3617d160ade461c48a4ef6b1e2e2c011d
John Flournoy <uoflournoy@gmail.com>
2017-06-13 09:42:00 -0700

    title change

--- rw_model/app.R
+++ rw_model/app.R
@@ -14,7 +14,7 @@ library(tidyverse)
 ui <- fluidPage(
    
    # Application title
-   titlePanel("Old Faithful Geyser Data"),
+   titlePanel("Associative Learning Simulation"),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
