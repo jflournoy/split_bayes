5e6fb5ec7da7ca5a1d0acb53c899bee7e60fb5bb
John Flournoy <uoflournoy@gmail.com>
2017-06-14 17:11:55 -0700

    more information in demo

--- rw_model/app.R
+++ rw_model/app.R
@@ -16,50 +16,70 @@ ui <- fluidPage(
    # Application title
    titlePanel("Associative Learning Simulation"),
    
+   withMathJax(),
+   
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
          sliderInput("xi",
-                     "Xi",
-                     min = -2,
-                     max = 2,
+                     HTML("\\(\\xi\\) <span style=\"font-weight: normal; font-style: italic;\">(noise)</span>"),
+                     min = -3,
+                     max = 3,
                      value = 0,
                      step=.05),
          sliderInput("ep",
-                     "Epsilon",
-                     min = -2,
-                     max = 2,
+                     HTML("\\(\\epsilon\\) <span style=\"font-weight: normal; font-style: italic;\">(learning rate)</span>"),
+                     min = -3,
+                     max = 3,
                      value = 0,
                      step=.05),
-         sliderInput("b",
-                     "Bias",
-                     min = -2,
-                     max = 2,
-                     value = 0,
-                     step=.05),
+         # sliderInput("b",
+         #             "Bias",
+         #             min = -2,
+         #             max = 2,
+         #             value = 0,
+         #             step=.05),
          sliderInput("rho",
-                     "Rho",
-                     min = -2,
-                     max = 2,
+                     HTML("\\(\\rho\\) <span style=\"font-weight: normal; font-style: italic;\">(inverse temperature)</span>"),
+                     min = -3,
+                     max = 3,
                      value = 0,
                      step=.05)
       ),
       
       # Show a plot of the generated distribution
       mainPanel(
-         plotOutput("trialsPlot")
+         plotOutput("trialsPlot"),
+         helpText("RW Equation: \n
+                        $$p(a_t|s_t) = \\text{logit}^{-1}\\Big(Q_{t-1}(a_{t},s_{t}) + \\epsilon\\big(\\rho r_{t} - Q_{t-1}(a_{t},s_{t})\\big)\\Big)\\cdot (1-\\xi) + \\frac{\\xi}{2}$$"),
+         uiOutput('rw_eq')
+         # ,
+         # tableOutput('runTable')
       )
    )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-  p_right <- data.frame(expand.grid(cue=1:6, reward = c(1,5)), pcorrect_if_pressed_r=c(rep(.2,3), rep(.8,3)))
   
-  Trials <- p_right[sample(1:dim(p_right)[1], size = 8*48, replace = T),]
-  Trials$crct_if_right <- rbinom(dim(Trials)[1], size = 1, prob = Trials$pcorrect_if_pressed_r)
-  Trials$outcome_r <- Trials$crct_if_right*Trials$reward
-  Trials$outcome_l <- (1-Trials$crct_if_right)*Trials$reward
+  generateTrials <- function() {
+    p_right <- data.frame(expand.grid(cue=1:2, reward = c(1,5)), pcorrect_if_pressed_r=c(rep(.2,1), rep(.8,1)))
+    
+    cue1Indxs <- sample(c(1,3), size = 60, replace = T)
+    cue2Indxs <- sample(c(2,4), size = 60, replace = T)
+    
+    manyTrialIndxs <- c(cue1Indxs,cue2Indxs)
+    
+    manyTrialIndxsShuffled <- manyTrialIndxs[sample(1:length(manyTrialIndxs), 
+                                                    size = length(manyTrialIndxs), 
+                                                    replace = F)]
+    
+    Trials <- p_right[manyTrialIndxsShuffled,]
+    Trials$crct_if_right <- rbinom(dim(Trials)[1], size = 1, prob = Trials$pcorrect_if_pressed_r)
+    Trials$outcome_r <- Trials$crct_if_right*Trials$reward
+    Trials$outcome_l <- (1-Trials$crct_if_right)*Trials$reward
+    return(Trials)
+  }
   
   inv_logit <- function(x) exp(x)/(1+exp(x))
   Phi_approx <- function(x) pnorm(x)
@@ -113,34 +133,59 @@ server <- function(input, output) {
   }
   
   plot_RW_run <- function(trials, mu_p){
-    single_run <- rw_strategy(trialdf = Trials,
+    single_run <- rw_strategy(trialdf = trials,
                               mu_p = mu_p)
     
     aplot <- single_run %>%
-      filter(cue %in% c(1,4)) %>%
       mutate(cue = factor(cue)) %>%
       group_by(cue) %>%
-      mutate(t = 1:n(), last_outcome = as.numeric( ifelse(lag(pressed_r) == 1 & lag(outcome) > 0, 1,
-                                                          ifelse(lag(pressed_r) == 1 & lag(outcome) == 0, .1,
-                                                                 ifelse(lag(pressed_r) == 0 & lag(outcome) > 0, 0,
-                                                                        .9)))),
+      mutate(t = 1:n(), last_outcome = as.numeric( ifelse(lag(pressed_r) == 1 & lag(outcome) == 5, 1,
+                                                          ifelse(lag(pressed_r) == 1 & lag(outcome) == 1, .95,
+                                                                 ifelse(lag(pressed_r) == 1 & lag(outcome) == 0, .1,
+                                                                        ifelse(lag(pressed_r) == 0 & lag(outcome) == 5, 0,
+                                                                               ifelse(lag(pressed_r) == 0 & lag(outcome) == 1, .05,
+                                                                                      .9)))))),
              last_press = lag(pressed_r)) %>%
       ggplot(aes(x = t, y = pGo)) + 
+      geom_line(stat = 'smooth', method = 'gam', formula = y ~ s(x, k = 8,  bs = "cs"), alpha = .5) + 
       geom_segment(aes(xend = t, yend = last_outcome), alpha = .1, color = 'black') + 
       geom_point(aes(y = last_outcome, shape = factor(last_press))) + 
       scale_shape_manual(values = c(25,24), name = 'Last press was...', breaks = c(1,0), labels = c('Right', 'left')) +
-      scale_y_continuous(breaks = c(0,1), labels = c('left', 'right'))+
+      scale_y_continuous(breaks = c(0,.5, 1), labels = c('left', '', 'right'))+
       geom_point() + 
-      geom_line(stat = 'smooth', method = 'gam', formula = y ~ s(x, k = 8,  bs = "cs")) + 
-      facet_wrap(pcorrect_if_pressed_r~cue, nrow = 2)+
-      theme(panel.background = element_blank(), strip.text = element_blank())
-    return(aplot)
+      facet_wrap(~cue, nrow = 2)+
+      theme(panel.background = element_blank(), 
+            # strip.text = element_blank(),
+            strip.background = element_rect(fill = '#eeeeee'))+
+      labs(y = "More likely to press...", x = 'Trial number')
+    return(list(plot = aplot, runData = single_run))
   }
   
+  someTrials <- generateTrials()
+  
+  simulatedTrials <- reactive({
+    plot_RW_run(trials = someTrials,
+                mu_p = c(xi = input$xi, ep = input$ep, b = 0, rho = input$rho))
+  })
+  
   output$trialsPlot <- renderPlot({
-      plot_RW_run(trials = Trials,
-                  mu_p = c(xi = input$xi, ep = input$ep, b = input$b, rho = input$rho))
+    simulatedTrials()$plot
    })
+  
+  output$rw_eq <- renderUI({
+    xi <- Phi_approx( input$xi )
+    ep <- Phi_approx( input$ep )
+    b <- input$b
+    rho <- exp( input$rho )
+    
+    withMathJax(sprintf("RW Equation with transformed values: \n
+                        $$p(a_t|s_t) = \\text{logit}^{-1}\\Big(Q_{t-1}(a_{t},s_{t}) + %.02f\\big(%.02f  r_{t} - Q_{t-1}(a_{t},s_{t})\\big)\\Big)\\cdot (1-%.02f) + \\frac{%.02f}{2}$$",
+                        ep, rho, xi, xi))
+  })
+  
+  output$runTable <- renderTable({
+    simulatedTrials()$runData
+  })
 }
 
 # Run the application 
